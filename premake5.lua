f7625a436954f05e1bf3bbd6519d4bf3b0f76e05
unknown <qhliang1002@gmail.com>
2026-02-10 22:17:14 +0000

    add premake

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "Twist"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	filter "system:windows"
+		buildoptions {"/utf-8"}
+	
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Twist"
+	location "Twist"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "on"
+		systemversion "10.0"
+
+		defines
+		{
+			"TW_PLATFORM_WINDOWS", 
+			"TW_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/DumbGame")
+		}
+
+	filter "configurations:Debug"
+		defines "TW_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "TW_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "TW_DIST"
+		optimize "On"
+
+
+project "DumbGame"
+	location "DumbGame"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Twist/vendor/spdlog/include",
+		"Twist/src"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "on"
+		systemversion "10.0"
+
+		defines
+		{
+			"TW_PLATFORM_WINDOWS" 
+		}
+
+	links
+	{
+		"Twist"
+	}
+
+	filter "configurations:Debug"
+		defines "TW_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "TW_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "TW_DIST"
+		optimize "On"
